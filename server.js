54b6553139d6a0838b4aa5c4796c030f8475a112
matheuscassemiro1 <127998481+matheuscassemiro1@users.noreply.github.com>
2023-11-28 16:24:45 -0300

    allow routes origin *

--- server.js
+++ server.js
@@ -19,13 +19,15 @@ const fs = require('fs/promises')
 
 app = express()
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-});
 
 
 app.use(express.static(__dirname + '/public'))
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Cookie");
+    next();
+  });
 
 app.disable('x-powered-by');
 
@@ -45,16 +47,9 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(`/api`, apiRouter);
-app.get('/', async function (req, res, next) {
 
-    res.sendFile(path.join(__dirname, './public/pages/index.html'))
-
-
-})
 app.use(cookieparser());
-app.get('/login', async (req, res, next) => {
-    res.sendFile(path.join(__dirname, `./public/pages/login.html`))
-})
+
 
 //app.use('/imagens', imgRouter)
 
