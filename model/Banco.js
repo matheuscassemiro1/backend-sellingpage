df1c5a980fc8017ffaa279665188071f93e1e308
matheuscassemiro1 <127998481+matheuscassemiro1@users.noreply.github.com>
2023-12-25 11:38:26 -0300

    deploy

--- model/Banco.js
+++ model/Banco.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
 
-const sequelize = new Sequelize("mm", "root", "", {
+const sequelize = new Sequelize("mm", "multimarcas", "xhtdwu2", {
     host: 'localhost',
     dialect: 'mysql',
     port: '3306',
