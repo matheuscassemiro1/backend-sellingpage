9bec5972735d46cb102988585ec624a1001c154c
matheuscassemiro1 <matheus.simscampos@gmail.com>
2024-06-13 21:34:47 -0300

    fix: fix .env pattern

--- model/Banco.js
+++ model/Banco.js
@@ -1,10 +1,11 @@
 const Sequelize = require("sequelize");
 const pg = require("pg")
+require('dotenv')
 
-const sequelize = new Sequelize("verceldb", "default", "dLMnYOb0T2tZ", {
-    host: 'ep-dawn-band-a4m5hehl-pooler.us-east-1.aws.neon.tech',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: 'postgres',
-    port: '5432',
+    port: process.env.DB_PORT,
     timezone: '-03:00',
     dialectOptions: {
         dateStrings: true,
